2b1085b6dbc82837f90558bda1e81193d3b09fef
mstclair <stclairm@spu.edu>
2017-07-18 06:48:32 -0700

    all locations listed

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ var pike = {
     }
   },
   render:function(){
-    var alkiUL = document.getElementById('alki');
+    var pikUL = document.getElementById('pik');
     for(var i = 0; i < timeOfDay.length; i++)
     {
-      var liEl = document.createElement('li');
-      liEl.textContent = timeOfDay[i] + ': ' + pike.custRand();
-      alkiUL.appendChild(liEl);
-      console.log(timeOfDay[i] + ':' + pike.custRand());
+      var liPi  = document.createElement('li');
+      liPi.textContent = timeOfDay[i] + ': ' + pike.custRand();
+      pikUL.appendChild(liPi);
+      //console.log(timeOfDay[i] + ':' + pike.custRand());
     }
   }
 }; ///end of function
@@ -38,25 +38,140 @@ pike.render();
 
 console.log(pike.cookiesPerHour());
 
-// //var alki = {
-//    name: 'Alki',
-//    seagullCount: [34, 56, 78, 14, 3000, 9000, 1200],
-//    render: function(){
-//      var alkiUL = document.getElementById('alki');
-//      // This will render the seagull data array to the DOM
-//      // We'll need a 'for' loop to iterate over the array
+////seaTacA
 
-//      for(var i = 0; i < this.seagullCount.length; i++){
-//        // There are three parts to this process:
-//        // 1. Create an element
-//        var liEl = document.createElement('li');
-//        // 2. Give it content
-//        liEl.textContent = days[i] + ': ' + this.seagullCount[i];
-//        // 3. Append it to a certain place in the DOM
-//        // parentElement.appendChild(childElement)
-//        alkiUL.appendChild(liEl);
-//      }
-//    }
-//  };
-//
-//  alki.render();
+var seaTacA = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  custRand: function(){
+
+    var randCustAmt = Math.floor(Math.random() * ((seaTacA.maxCust - seaTacA.minCust) + 1) + seaTacA.minCust) * seaTacA.avgCookies;
+
+
+    return randCustAmt;//   //returns random customer amt to be multiplied by avg cookie for 6 am
+  },
+  //
+  // ///////////getting avg cookies per hour
+  //
+  cookiesPerHour: function (){
+    for(var i = 0; i < timeOfDay.length; i++)
+    {
+      console.log(timeOfDay[i] + ':' + seaTacA.custRand());
+    }
+  },
+  render:function(){
+    var seaUL = document.getElementById('seaTac');
+    for(var i = 0; i < timeOfDay.length; i++)
+    {
+      var liSea = document.createElement('li');
+      liSea.textContent = timeOfDay[i] + ': ' + seaTacA.custRand();
+      seaUL.appendChild(liSea);
+      //console.log(timeOfDay[i] + ':' + pike.custRand());
+    }
+  }
+}; ///end of function
+
+seaTacA.render();
+
+var seaCent = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  custRand: function(){
+
+    var randCustAmt = Math.floor(Math.random() * ((seaCent.maxCust - seaCent.minCust) + 1) + seaCent.minCust) * seaCent.avgCookies;
+
+
+    return randCustAmt;//   //returns random customer amt to be multiplied by avg cookie for 6 am
+  },
+  //
+  // ///////////getting avg cookies per hour
+  //
+  cookiesPerHour: function (){
+    for(var i = 0; i < timeOfDay.length; i++)
+    {
+      console.log(timeOfDay[i] + ':' + seaCent.custRand());
+    }
+  },
+  render:function(){
+    var seaCUl = document.getElementById('seaCent');
+    for(var i = 0; i < timeOfDay.length; i++)
+    {
+      var liSeaC = document.createElement('li');
+      liSeaC.textContent = timeOfDay[i] + ': ' + seaCent.custRand();
+      seaCUl.appendChild(liSeaC);
+      //console.log(timeOfDay[i] + ':' + pike.custRand());
+    }
+  }
+}; ///end of function
+
+seaCent.render();
+
+var caphill = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  custRand: function(){
+
+    var randCustAmt = Math.floor(Math.random() * ((caphill.maxCust - caphill.minCust) + 1) + caphill.minCust) * caphill.avgCookies;
+
+
+    return randCustAmt;//   //returns random customer amt to be multiplied by avg cookie for 6 am
+  },
+  //
+  // ///////////getting avg cookies per hour
+  //
+  cookiesPerHour: function (){
+    for(var i = 0; i < timeOfDay.length; i++)
+    {
+      console.log(timeOfDay[i] + ':' + caphill.custRand());
+    }
+  },
+  render:function(){
+    var capUl = document.getElementById('cap');
+    for(var i = 0; i < timeOfDay.length; i++)
+    {
+      var liCap = document.createElement('li');
+      liCap.textContent = timeOfDay[i] + ': ' + caphill.custRand();
+      capUl.appendChild(liCap);
+      //console.log(timeOfDay[i] + ':' + pike.custRand());
+    }
+  }
+}; ///end of function
+
+caphill.render();
+
+var alki = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+  custRand: function(){
+
+    var randCustAmt = Math.floor(Math.random() * ((alki.maxCust - alki.minCust) + 1) + alki.minCust) * alki.avgCookies;
+
+
+    return randCustAmt;//   //returns random customer amt to be multiplied by avg cookie for 6 am
+  },
+  //
+  // ///////////getting avg cookies per hour
+  //
+  cookiesPerHour: function (){
+    for(var i = 0; i < timeOfDay.length; i++)
+    {
+      console.log(timeOfDay[i] + ':' + alki.custRand());
+    }
+  },
+  render:function(){
+    var alkUl = document.getElementById('alk');
+    for(var i = 0; i < timeOfDay.length; i++)
+    {
+      var liAlk = document.createElement('li');
+      liAlk.textContent = timeOfDay[i] + ': ' + alki.custRand();
+      alkUl.appendChild(liAlk);
+      //console.log(timeOfDay[i] + ':' + pike.custRand());
+    }
+  }
+}; ///end of function
+
+alki.render();
